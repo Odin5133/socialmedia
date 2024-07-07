0473d3587ad4149741a3325c5f9817e6f6c58d76
Odin5133 <zanjadatharva@protonmail.com>
2024-07-07 13:42:01 +0530

    Added /user endpoint

--- src/Login/SignIn.jsx
+++ src/Login/SignIn.jsx
@@ -12,36 +12,52 @@ function SignIn() {
     setPassword(e.target.value);
   };
 
-  useEffect(() => {
-    axios
-      .post("http://127.0.0.1:8000/api/refresh/")
-      .then((response) => {
-        Cookies.set("accessToken", response.data.token, {
-          expires: 1,
-          secure: true,
+  // useEffect(() => {
+  //   console.log(`Bearer ${Cookies.get("accessToken")}`);
+  //   fetch("http://127.0.0.1:8000/api/user/", {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization: `Bearer ${Cookies.get("accessToken")}`,
+  //     },
+  //   })
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => console.log(data))
+  //     .catch((error) =>
+  //       console.error(
+  //         "There has been a problem with your fetch operation:",
+  //         error
+  //       )
+  //     );
+  // });
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    fetch("http://127.0.0.1:8000/api/login/", {
+      method: "POST",
+      credentials: "include", // Necessary for cookies to be sent with requests
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login successful:", data);
+        // Assuming the accessToken is still being sent in the response body
+        Cookies.set("accessToken", data.token, {
+          expires: 1, // 1 day
         });
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-
-  const handleSignIn = () => {
-    axios
-      .post(
-        "http://127.0.0.1:8000/api/login/",
-        {
-          email: username,
-          password: password,
-        },
-        { withCredentials: true }
-      )
-      .then((response) => {
-        console.log(response.data);
-        Cookies.set("accessToken", response.data.token, {
-          expires: 1,
-          secure: true,
-        });
+      .catch((error) => {
+        console.error("Login error:", error);
       });
   };
 
@@ -55,29 +71,31 @@ function SignIn() {
           Dive into your personalized haven
         </div>
         <div className=" mt-24 text-text">
-          <div className="">Username / E-Mail</div>
-          <input
-            type="text"
-            className=" mt-2 rounded-md px-2 py-1 w-[90%] bg-background border-2 border-primary text-text focus:outline-none focus:border-accent"
-            placeholder="InvincibleMe3"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-          <div className=" mt-8">Password</div>
-          <input
-            type="password"
-            className="mt-2 rounded-md px-2 py-1 w-[90%] bg-background border-2 border-primary text-text focus:outline-none focus:border-accent"
-            placeholder="SuperSecretPassword123"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <br />
-          <button
-            className="mt-12 rounded-lg bg-accent px-4 py-1 text-xl border-2 border-primary text-text hover:bg-accent-hover focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"
-            onClick={handleSignIn}
-          >
-            Sign In
-          </button>
+          <form onSubmit={handleSignIn}>
+            <div className="">Username / E-Mail</div>
+            <input
+              type="text"
+              className=" mt-2 rounded-md px-2 py-1 w-[90%] bg-background border-2 border-primary text-text focus:outline-none focus:border-accent"
+              placeholder="InvincibleMe3"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            <div className=" mt-8">Password</div>
+            <input
+              type="password"
+              className="mt-2 rounded-md px-2 py-1 w-[90%] bg-background border-2 border-primary text-text focus:outline-none focus:border-accent"
+              placeholder="SuperSecretPassword123"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <br />
+            <button
+              className="mt-12 rounded-lg bg-accent px-4 py-1 text-xl border-2 border-primary text-text hover:bg-accent-hover focus:outline-none focus:ring-2 focus:ring-accent focus:border-transparent"
+              type="submit"
+            >
+              Sign In
+            </button>
+          </form>
         </div>
       </div>
     </div>
